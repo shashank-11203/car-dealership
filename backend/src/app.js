6c6f49f9686eacc861a982a19df273db4a600b34
shashank410 <shashanksolanki020@gmail.com>
2026-07-23 10:41:10 +0530

    test end point

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,6 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("Car Dealership Backend Running");
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/vehicles", vehicleRoutes);
 
